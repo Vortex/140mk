e9b157e3f12dc7395bbdadfc5b2155ea4e18abc6
Peco Danajlovski <vortexmk@gmail.com>
2010-01-08 00:51:54 +0100

    fetch sidebar tweets in descending order (newest first)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -26,7 +26,7 @@ class WelcomeController < ApplicationController
   private
 
   def get_filtered_tweets
-    @filtered_tweets = Tweet.find(:all, :conditions => ["text like ?", "%#fail%"])
+    @filtered_tweets = Tweet.find(:all, :conditions => ["text like ?", "%#fail%"], :order => 'twitter_id DESC')
   end
 
 

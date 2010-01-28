335e5dee1a17ffb3832c86c946e2660f8afdcec7
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-01-28 01:02:41 +0100

    Added localization for pagination links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  include WillPaginate::ViewHelpers 
+  def will_paginate_with_i18n(collection, options = {}) 
+    will_paginate_without_i18n(collection, options.merge(:previous_label => I18n.t('pagination.previous'), :next_label => I18n.t('pagination.next'))) 
+  end
+  alias_method_chain :will_paginate, :i18n
 end

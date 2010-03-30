0e132119699bcdc8a065f94270949aec323dad64
Peco <peco@workarion.(none)>
2010-03-30 09:38:34 +0200

    use 6 categories for front page instead of 4

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,7 +2,7 @@ ConsumerConfig = YAML.load(File.read(Rails.root + 'config' + 'twitter.yml'))
 
 G140 = {
   :site_name => "140.mk",
-  :front_page_lists_count => 4,
+  :front_page_lists_count => 6,
   :tweets_per_list => 5,
   :users_per_list => 5,
   :max_lists => 5,
@@ -10,5 +10,5 @@ G140 = {
   :tweets_per_page => 10,
   :categories_per_page => 16,
   :users_per_page => 10,
-  :subscriptions_per_user => 5
+  :subscriptions_per_user => 6
 }

39a262ea1fa15ec5ee2055cc64570574f1f81bfc
Peco Danajlovski <vortexmk@gmail.com>
2010-06-01 01:29:23 +0200

    new favicon, bugfixes

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,7 +2,7 @@ class AboutController < ApplicationController
 
   def index
     user = User.find_by_screen_name(G140[:prod_account])
-    @tweets = user.tweets
+    @tweets = user.tweets.all(:limit => 6)
   end
 
 end

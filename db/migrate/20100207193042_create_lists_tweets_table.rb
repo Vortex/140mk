1f28449da47bdb3acea92769ac2a3a824c49406c
Peco Danajlovski <vortexmk@gmail.com>
2010-06-06 22:57:55 +0200

    fixing migrations

--- db/migrate/20100207193042_create_lists_tweets_table.rb
+++ db/migrate/20100207193042_create_lists_tweets_table.rb
@@ -1,18 +1,18 @@
 class CreateListsTweetsTable < ActiveRecord::Migration
   def self.up
     create_table :lists_tweets, :id => false do |t|
-      t.integer :category_id
+      t.integer :list_id
       t.integer :tweet_id
     end
 
-    add_index :lists_tweets, [:category_id, :tweet_id]
+    add_index :lists_tweets, [:list_id, :tweet_id]
 
     # For existing test database
     # Link all tweets with the lists to which user is subscribed to
     User.transaction do
       User.find(:all, :select => "users.id, lists.id, subscriptions.user_id, subscriptions.list_id", :include => :lists).each do |user|
         user.tweets.each do |tweet|
-          user.lists.each do |list|
+          user.categories.each do |list|
             list.tweets << tweet
           end
         end

d37ef3b95fffd463a3fa9b00f656b774be5a7427
Peco Danajlovski <vortexmk@gmail.com>
2011-05-17 06:37:11 +0200

    fixed the titles and seo stuff

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -5,15 +5,18 @@
 module LayoutHelper
   def title(page_title, show_title = true)
     @content_for_title = page_title.to_s
+    content_for(:title) { @content_for_title }
     @show_title = show_title
   end
 
   def seo_description(description_text)
     @content_for_seo_description = description_text.to_s
+    content_for(:seo_description) { @content_for_seo_description }
   end
 
   def seo_keywords(keywords)
     @content_for_seo_keywords = keywords.to_s
+    content_for(:seo_keywords) { @content_for_seo_keywords }
   end
 
   def show_title?

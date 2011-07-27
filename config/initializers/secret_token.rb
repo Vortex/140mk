f33da06f9256e9de35f780f4451d65ea1175e299
Peco Danajlovski <dan@panter.ch>
2011-07-27 11:01:52 +0200

    Hope, a new beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T140mkv2::Application.config.secret_token = '0de64529141b06067176d4dfb14453f351b5434a04bcee0a9e86508808473493ab5d1cbf1ec8dd320cac9f9fa93c84668121221a854a7239ab382ababf8d3c20'
